33fbf857ffbc05f9ab3661135a35bb356bc49d2e
Hatim Mustafa <hatimmustafa108@gmail.com>
2025-03-07 00:55:59 +0500

    Grid Placement Done

--- lib/game_view.dart
+++ lib/game_view.dart
@@ -11,26 +11,55 @@ class _GameViewState extends State<GameView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(""),
+        backgroundColor: const Color.fromARGB(255, 255, 252, 233),
+      ),
       body: Center(
         child: SizedBox(
-          width: 250,
-          height: 300,
-          child: GridView.count(
-            crossAxisCount: 4,
-            children: List.generate(16, (index) {
-              return Container(
-                child: Text("2"),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 2,
-                  ),
-                ),
-              ); // Your tile widget
-            }),
+          width: MediaQuery.of(context).size.width - 20,
+          height: MediaQuery.of(context).size.width - 21,
+          child: Container(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: GridView.count(
+                crossAxisCount: 4,
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                children: List.generate(16, (index) {
+                  return Container(
+                    child: Center(
+                      child: Text(
+                        "2",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromRGBO(0x74, 0x6D, 0x63, 1),
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color.fromRGBO(0xBB, 0xAD, 0xA0, 1),
+                        width: 4,
+                      ),
+                      color: Color.fromRGBO(0xEE, 0xE3, 0xDD, 1),
+                    ),
+                  ); // Your tile widget
+                }),
+              ),
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: const Color.fromRGBO(187, 173, 160, 1),
+                width: 4,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
         ),
       ),
+      backgroundColor: const Color.fromARGB(255, 255, 252, 233),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:game2048/game_view.dart';
-import 'package:game2048/routes.dart';
+import 'package:game2048/constants/routes.dart';
 
 void main() {
   runApp(
@@ -30,7 +30,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(
-      //   title: const Text("2048"),
+      //   title: const Text(""),
       //   backgroundColor: const Color.fromARGB(255, 255, 252, 233),
       // ),
       body: Center(
@@ -38,22 +38,37 @@ class _HomePageState extends State<HomePage> {
           mainAxisSize: MainAxisSize.min,
           children: [
             SizedBox(
-              width: 200,
-              child: TextButton(
-                onPressed: () async {
-                  Navigator.of(context).pushNamed(GameRoute);
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 197, 73, 45),
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+              width: 250,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: Image.asset("assets/preview_image.png"),
                   ),
-                ),
-                child: const Text(
-                  'START GAME',
-                  style: TextStyle(fontSize: 18),
-                ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  SizedBox(
+                    width: 250,
+                    height: 50,
+                    child: TextButton(
+                      onPressed: () async {
+                        Navigator.of(context).pushNamed(GameRoute);
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 197, 73, 45),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: const Text(
+                        'START GAME',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
